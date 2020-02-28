bd0ab31e2c875c9e7a2c116eba6070fbac9a0717
heroicmuffin <theoneandonlypeleg@gmail.com>
2020-02-28 17:05:38 +0200

    bbundle

---
+++ project3-client/build/precache-manifest.f82823ecb190e1fad5930ddeedbf8878.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fdf8a19fec0d89f50f8ddf4a808a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d6ea50a044403163374",
+    "url": "/static/css/2.34845db8.chunk.css"
+  },
+  {
+    "revision": "3492115065fd7b770615",
+    "url": "/static/css/main.d786848b.chunk.css"
+  },
+  {
+    "revision": "1d6ea50a044403163374",
+    "url": "/static/js/2.23135d0a.chunk.js"
+  },
+  {
+    "revision": "3f375e71f02c243d2d110c0d9758f8ab",
+    "url": "/static/js/2.23135d0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3492115065fd7b770615",
+    "url": "/static/js/main.87be4f4c.chunk.js"
+  },
+  {
+    "revision": "aaf8e8004e135c07ec59",
+    "url": "/static/js/runtime-main.ae165a6a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
