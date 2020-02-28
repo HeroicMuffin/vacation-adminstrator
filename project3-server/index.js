eb7bba3eee923d7520822f0a7b5ed7ee9f86fc95
heroicmuffin <theoneandonlypeleg@gmail.com>
2020-02-28 16:12:52 +0200

    add bundle

--- project3-server/index.js
+++ project3-server/index.js
@@ -8,12 +8,31 @@ const vacationDB = require("./routes/vacationDB");
 const cors = require("cors");
 const app = express();
 
+const path = require('path')
+
+
+
+const root = path.resolve(
+  __dirname,
+  '..',
+  "project3-client",
+  'build'
+)
+
+
 app.use(cors());
 app.use(bodyParser.json());
+
 
 app.use("/", routing);
 app.use("/", vacationDB);
 app.use("/admin", administrator);
+
+app.use(express.static(root));
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root })
+})
 
 app.listen(3200, () => {
   console.log("listening  to: " + process.env.PORT);
